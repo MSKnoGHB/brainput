e38eb34f7f45ee400861c3ae5a66435d28675958
政木 佑哉 <youmac@MSKnoMBA.local>
2026-05-17 22:58:50 +0900

    add:created a post form

--- notebook/views.py
+++ notebook/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from .models import Category
 
 # Create your views here.
 
 def index(request):
   return render(request, 'notebook/index.html')
+
+def new(request):
+  categories = Category.objects.all()
+  return render(request, 'notebook/new.html', {'categories': categories})
+
+def create(request):
+  
+  return render()

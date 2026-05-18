682bab80a3f6a15908519b318ffec473a2e7c799
政木 佑哉 <youmac@MSKnoMBA.local>
2026-05-18 14:15:46 +0900

    add:display for saved data

--- notebook/views.py
+++ notebook/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, redirect
-from .models import Category
+from .models import Category, SubCategory, Note
 from .forms import NoteForm
 
 # Create your views here.
 
 def index(request):
-  return render(request, 'notebook/index.html')
+  notes = Note.objects.all()
+  return render(request, 'notebook/index.html',{'notes': notes})
 
 def new(request):
   categories = Category.objects.all()
-  return render(request, 'notebook/new.html', {'categories': categories})
+  sub_categories = SubCategory.objects.all()
+  return render(request, 'notebook/new.html', {'categories': categories, 'sub_categories': sub_categories})
 
 def create(request):
   form = NoteForm(request.POST)

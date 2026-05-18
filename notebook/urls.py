719ffc7d3f81336c23fa95520ee6aba525c60aba
政木 佑哉 <youmac@MSKnoMBA.local>
2026-05-18 16:53:53 +0900

    add:note CRUD views and URL routing

--- notebook/urls.py
+++ notebook/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
   path('', views.index, name='index'),
   path('new/', views.new, name='new'),
-  path('create/', views.create, name='create')
+  path('create/', views.create, name='create'),
+  path('<int:id>/show/', views.show, name='show'),
+  path('<int:id>/edit/', views.edit, name='edit'),  
+  path('<int:id>/update/', views.update, name='update'),
+  path('<int:id>/destroy/', views.destroy, name='destroy')
 ]
--- notebook/views.py
+++ notebook/views.py
@@ -4,15 +4,18 @@ from .forms import NoteForm
 
 # Create your views here.
 
+#一覧画面(ルート画面)
 def index(request):
   notes = Note.objects.all()
   return render(request, 'notebook/index.html',{'notes': notes})
 
+#新規作成画面
 def new(request):
   categories = Category.objects.all()
   sub_categories = SubCategory.objects.all()
   return render(request, 'notebook/new.html', {'categories': categories, 'sub_categories': sub_categories})
 
+#新規作成処理
 def create(request):
   form = NoteForm(request.POST)
   if form.is_valid():
@@ -20,4 +23,41 @@ def create(request):
     return redirect('index')
   else:
     categories = Category.objects.all()
-    return render(request, 'notebook/new.html', {'categories': categories, 'form':form})
+    sub_categories = SubCategory.objects.all()
+    return render(request, 'notebook/new.html', {'categories': categories, 'sub_categories': sub_categories, 'form':form})
+  
+#詳細画面
+def show(request, id):
+  note = Note.objects.get(id=id)
+  return render(request, 'notebook/show.html', {'note':note})
+
+#編集画面
+def edit(request, id):
+  note = Note.objects.get(id=id)
+  categories = Category.objects.all()
+  sub_categories = SubCategory.objects.all()
+  return render(request, 'notebook/edit.html',{ 'note':note, 'categories': categories, 'sub_categories': sub_categories})
+  
+#更新処理
+def update(request, id):
+  note = Note.objects.get(id=id)
+  #instans=noteでPATCHになる
+  form = NoteForm(request.POST, instance=note)
+  if form.is_valid():
+    form.save()
+    return redirect('show', id=note.id)
+  else:
+    categories = Category.objects.all()
+    sub_categories = SubCategory.objects.all()
+    return render(request, 'notebook/edit.html', {'note': note, 'categories': categories, 'sub_categories': sub_categories, 'form':form})
+  
+#削除処理
+def destroy(request, id):
+  note = Note.objects.get(id=id)
+  note.delete()
+  return redirect('index')
+
+
+    
+    
+    

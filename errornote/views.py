4babbc6ef3e33d8f5e1db4f80fc5a7cd5357cc44
MSKnoGHB <242181889+MSKnoGHB@users.noreply.github.com>
2026-05-30 23:26:50 +0900

    add: errornote views skeleton

--- errornote/views.py
+++ errornote/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.db.models import Q 
+from .models import ErrorNote
+from .forms import ErrorNoteForm
 
 # Create your views here.
+def dashboard(request):
+  
+  return render(request, '',{})
+
+
+
+
+def index(request):
+
+  return render(request, '',{})
+
+
+
+def create(request):
+  
+  return render(request, '',{})
+
+  
+  
+def show(request, id):
+  
+  return render(request, '',{})
+
+  
+  
+def update(request, id):
+  
+  return render(request, '',{})
+
+  
+  
+def destroy(request, id):
+  
+  return render(request, '',{})

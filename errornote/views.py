ea1f79b3cbd5291e5ae2147ed39969b46f11cb53
MSKnoGHB <242181889+MSKnoGHB@users.noreply.github.com>
2026-05-31 14:10:20 +0900

    add: complete views.py

--- errornote/views.py
+++ errornote/views.py
@@ -5,37 +5,35 @@ from .models import ErrorNote
 from .forms import ErrorNoteForm
 
 # Create your views here.
-def dashboard(request):
-  
-  return render(request, '',{})
-
-
-
 
 def index(request):
-
-  return render(request, '',{})
-
+  error_notes = ErrorNote.objects.all()
+  return render(request, 'errornote/index.html', {'error_notes': error_notes})
 
 
 def create(request):
-  
-  return render(request, '',{})
+  error_note_form = ErrorNoteForm(request.POST or None)
+  if request.method == 'POST' and error_note_form.is_valid():
+    error_note = error_note_form.save()
+    return redirect('errornote:show', id=error_note.id)
+  return render(request, 'errornote/create.html',{'error_note_form':error_note_form})
 
-  
-  
+
 def show(request, id):
-  
-  return render(request, '',{})
+  error_note = ErrorNote.objects.get(id=id)
+  return render(request, 'errornote/show.html',{'error_note':error_note})
 
-  
   
 def update(request, id):
-  
-  return render(request, '',{})
+  error_note = ErrorNote.objects.get(id=id)
+  error_note_form = ErrorNoteForm(request.POST, instance=error_note)
+  if request.method =='POST' and error_note_form.is_valid():
+    error_note_form.save()
+    return redirect('errornote:show',{'error_note':error_note, 'error_note_form':error_note_form})
+  return render(request, 'errornote/update.html',{'error_note':error_note, 'error_note_form':error_note_form})
 
   
-  
 def destroy(request, id):
-  
-  return render(request, '',{})
+  error_note = ErrorNote.objects.get(id=id) 
+  error_note.delete()
+  return redirect('errornote:index')
